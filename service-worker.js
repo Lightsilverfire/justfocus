9380899f2d88407511afa43c2f0975f562d88391
Lightsilverfire <bliksem@wmmt.nl>
2022-04-05 11:22:53 +0200

    testing offline

--- service-worker.js
+++ service-worker.js
@@ -29,6 +29,22 @@ self.addEventListener("install",(installing)=>{
   
   self.addEventListener("fetch",(fetching)=>{   
     console.log("Service Worker: User threw a ball, I need to fetch it!");
+    fetching.respondWith(
+      caches.match(fetching.request.url).then((response)=>{
+        console.log("Service Worker: Fetching resource "+fetching.request.url);
+        return response||fetch(fetching.request).then((response)=>{
+          console.log("Service Worker: Resource "+fetching.request.url+" not available in cache");
+          return caches.open(cacheName).then((cache)=>{
+            console.log("Service Worker: Caching (new) resource "+fetching.request.url);
+            // OUTCOMMENT THIS LINE FOR ADDING DOWNLOADED RESOURCES TO YOUR CACHE: cache.put(fetching.request,response.clone());
+            return response;
+          });
+        }).catch(function(){      
+          console.log("Service Worker: Fetching online failed, HAALLPPPP!!!");
+          //Do something else with the request (for example: respond with a different cached file)
+        })
+      })
+    );
   });
   
   self.addEventListener("push",(pushing)=>{
