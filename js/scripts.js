d35aa8894519ac06c1b210675090c190c27d141a
ArjanElissen <arjanelissen@hotmail.com>
2022-04-06 10:41:56 +0200

    Buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 //See if the browser supports Service Workers, if so try to register one
-if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("service-worker.js").then(function(registering){
-      // Registration was successful
-      console.log("Browser: Service Worker registration is successful with the scope",registering.scope);
-    }).catch(function(error){
-      //The registration of the service worker failed
-      console.log("Browser: Service Worker registration failed with the error",error);
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js").then(function(registering) {
+        // Registration was successful
+        console.log("Browser: Service Worker registration is successful with the scope", registering.scope);
+    }).catch(function(error) {
+        //The registration of the service worker failed
+        console.log("Browser: Service Worker registration failed with the error", error);
     });
-  } else {
+} else {
     //The registration of the service worker failed
     console.log("Browser: I don't support Service Workers :(");
-  }
+}
+
+//Functie activate gif
+
+function playgif() {
+
+    document.getElementById('mainmenu').style.backgroundImage = "url(/images/start.gif)";
+    document.getElementById('buttonsdiv').style.display = "none"
+}
