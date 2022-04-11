87f2424e9bc4d30a1e830b113d674da2f85764dd
ArjanElissen <arjanelissen@hotmail.com>
2022-04-11 12:33:52 +0200

    classes hiding and showing by Tessa

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,48 @@ if ("serviceWorker" in navigator) {
 }
 
 //Functie activate gif
+document.getElementById("startButton").addEventListener("click", playgif);
+document.getElementById("continueButton").addEventListener("click", playgif);
+document.getElementById("hamburgermenu").addEventListener("click", hamburger);
+document.getElementById("popContinue").addEventListener("click", vervolg);
+
+
+let menuswitch = document.getElementById("hamburgermenu");
+let popmenu = document.getElementById("popupmenu");
+
+//Startup values and classes
+let opacitymenu = 0;
+let popswitch = 0;
+
+
+
+if (opacitymenu == 0) {
+    menuswitch.classList.add("hamburgerHide")
+    menuswitch.classList.remove("hamburgerShow")
+}
+
+if (popswitch == 0) {
+    popmenu.classList.remove("popShow")
+    popmenu.classList.add("popHide")
+}
 
 function playgif() {
-
+    menuswitch.classList.remove("hamburgerHide")
+    menuswitch.classList.add("hamburgerShow")
     document.getElementById('mainmenu').style.backgroundImage = "url(/images/start.gif)";
     document.getElementById('buttonsdiv').style.display = "none"
+}
+
+function hamburger() {
+    popmenu.classList.add("popShow")
+    popmenu.classList.remove("popHide")
+    menuswitch.classList.add("hamburgerHide")
+    menuswitch.classList.remove("hamburgerShow")
+}
+
+function vervolg() {
+    popmenu.classList.remove("popShow")
+    popmenu.classList.add("popHide")
+    menuswitch.classList.remove("hamburgerHide")
+    menuswitch.classList.add("hamburgerShow")
 }
