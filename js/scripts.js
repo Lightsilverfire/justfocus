150c37bc2eebef3612a3cc15293a504d12c3ba8a
ArjanElissen <arjanelissen@hotmail.com>
2022-04-19 10:32:02 +0200

    show character on start

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,7 @@ if ("serviceWorker" in navigator) {
 
 //Functie activate gif
 document.getElementById("startButton").addEventListener("click", playgif);
+document.getElementById('startButton').addEventListener("click", showcharacter);
 document.getElementById("continueButton").addEventListener("click", playgif);
 document.getElementById("menu").addEventListener("click", hamburger);
 document.getElementById("popContinue").addEventListener("click", vervolg);
@@ -25,6 +26,7 @@ let menuswitch = document.getElementById("hamburgermenu");
 let popmenu = document.getElementById("popupmenu");
 let quitbutton = document.getElementById("quityesno");
 let quitbutton2 = document.getElementById("popupquit");
+let character = document.getElementById("canvas");
 
 //Startup values and classes
 let opacitymenu = 0;
@@ -54,6 +56,10 @@ function playgif() {
     document.getElementById('buttonsdiv').style.display = "none"
 }
 
+function showcharacter() {
+    canvas.style.display = "block"
+}
+
 function hamburger() {
     popmenu.classList.add("popShow")
     popmenu.classList.remove("popHide")
@@ -77,6 +83,7 @@ function quit() {
 
 function backtostart() {
     window.location.href = 'https://justfocus.netlify.app/';
+    canvas.style.display = "none"
 }
 
 function backtomenu() {
@@ -92,18 +99,18 @@ img.src = 'images/lopen.png';
 img.onload = function() {
     init();
 };
- 
+
 //sprite op canvas zetten met goede frame
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 let touch;
- 
+
 const scale = 1;
 const width = 255;
 const height = 255;
 const scaledWidth = scale * width;
 const scaledHeight = scale * height;
- 
+
 function drawFrame(frameX, frameY, canvasX, canvasY) {
     ctx.drawImage(img,
         frameX * width, frameY * height, width, height,
@@ -115,40 +122,39 @@ const cycleLoop = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
 
 let currentLoopIndex = 0;
 let frameCount = 0;
- 
- //touch event, touchstart is je vinger op het scherm is, touchend is als je vinger niet meer op het scherm is
-document.getElementById("canvas").addEventListener("touchstart", function (){touch = true});
-document.getElementById("canvas").addEventListener("touchend", function (){touch = false});
+
+//touch event, touchstart is je vinger op het scherm is, touchend is als je vinger niet meer op het scherm is
+document.getElementById("canvas").addEventListener("touchstart", function() { touch = true });
+document.getElementById("canvas").addEventListener("touchend", function() { touch = false });
 
 
- //sprite laten lopen als het scherm aangeraakt wordt
+//sprite laten lopen als het scherm aangeraakt wordt
 function step() {
 
-  if (touch){
-    frameCount++; 
-    if (frameCount < 4) {
-        window.requestAnimationFrame(step);
-        return;
+    if (touch) {
+        frameCount++;
+        if (frameCount < 4) {
+            window.requestAnimationFrame(step);
+            return;
+        }
+        frameCount = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0, 0);
+        currentLoopIndex++;
+        if (currentLoopIndex >= cycleLoop.length) {
+            currentLoopIndex = 0;
+        }
     }
-    frameCount = 0;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0, 0);
-    currentLoopIndex++;
-    if (currentLoopIndex >= cycleLoop.length) {
+    //als er niet meer op het scherm gedrukt wordt, dan maakt hij het scherm leeg en zet de eerste frame op het canvas
+    else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawFrame(cycleLoop[0], 0, 0, 0);
         currentLoopIndex = 0;
     }
-  }
-  //als er niet meer op het scherm gedrukt wordt, dan maakt hij het scherm leeg en zet de eerste frame op het canvas
-  else
-  {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawFrame(cycleLoop[0], 0, 0, 0);
-    currentLoopIndex = 0;
-  }
-  window.requestAnimationFrame(step);
+    window.requestAnimationFrame(step);
 
 }
- 
+
 function init() {
-  window.requestAnimationFrame(step);
+    window.requestAnimationFrame(step);
 }
